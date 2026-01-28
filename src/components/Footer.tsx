ee89f8c493bfc80249528197674f19eb951e465c
Ravi Dhungel <ravi.dhungel@bd.com>
2026-01-28 08:14:43 -0800

    Add view counter to footer with High Altitude Hiking style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,9 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-content">
+                <div className="view-counter">
+                    <img src="https://hits.dwyl.com/nepalboy/travelnew.svg" alt="Total Views" />
+                </div>
                 <p>&copy; {new Date().getFullYear()} Traversing With Time. All rights reserved.</p>
             </div>
         </footer>
